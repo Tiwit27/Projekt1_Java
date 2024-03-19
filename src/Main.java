4adf6be9267954fd1a2f0c3f89101b9ec00f5883
RL <lojekrafal27@gmail.com>
2024-03-19 15:26:03 +0100

    Udoskonalenie programu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Punkt
+{
+    int x;
+    int y;
+    Punkt(int x,int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    Punkt PobierzWspolrzedne()
+    {
+        Punkt punkt = new Punkt(x,y);
+        return punkt;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        Punkt miejsceZerowe = new Punkt(15,0);
+        System.out.println(miejsceZerowe.x);
+        Punkt miejsce = miejsceZerowe.PobierzWspolrzedne();
+        System.out.println(miejsce.y);
     }
 }
